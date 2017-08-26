29e9a72656f16fe0ee816ebc046e23458420cca3
jiangxujin <432853096@qq.com>
2017-08-26 11:48:51 +0800

    Signed-off-by: jiangxujin <432853096@qq.com>

--- src/router.js
+++ src/router.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Router, Route } from 'dva/router';
-import IndexPage from './routes/IndexPage';
+
+import Index from "./routes/Index.js";
 
 function RouterConfig({ history }) {
   return (
-    <Router history={history}>
-      <Route path="/" component={IndexPage} />
+    <Router history={history}>     
+      <Route path="/" component={Index} />
     </Router>
   );
 }
